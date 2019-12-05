6f5051dd3fbe990d04718189eebed7f9722b22a4
Angelo <15258333462@qq.com>
2019-12-05 09:01:02 +0800

    - first push

---
+++ README.md
@@ -0,0 +1,42 @@
+# Cordova plugin for theme detection in ionic3
+
+### Clone Code From Git
+```
+$ git clone https://github.com/13546777510/theme-detection-ionic3.git
+```
+
+### Open project
+```
+$ cd theme-detection-ionic3
+```
+
+### Install npm
+```
+$ npm install
+```
+
+### Run on iOS / Android
+
+#### iOS
+```
+$ ionic cordova prepare ios
+```
+
+#### Android
+```
+$ ionic cordova prepare android
+```
+
+#### Possible questions from the console
+Platform ios is not installed! Would you like to install it? (Y/n)
+```
+$ y
+```
+
+### Open project in Xcode / Android studio
+
+#### Xcode
+Open path: `theme-detection-ionic3/platforms/ios`
+
+#### Android studio
+Open path: `theme-detection-ionic3/platforms/android`
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController} from 'ionic-angular';
+
+declare let cordova: any;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,42 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  matchMedia = {
+    isAvailable: null,
+    isDarkModeEnabled: null
+  };
 
+  cordovaPlugin = {
+    isAvailable: null,
+    isDarkModeEnabled: null
+  };
+
+  constructor(public navCtrl: NavController) {
+  }
+
+  ionViewDidEnter(){
+    this.doRefreshAppTheme();
+  }
+
+  doRefreshAppTheme(){
+    //By window.matchMedia -> matchMedia
+    let mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+    this.matchMedia.isAvailable = !!mediaQueryList;
+    let isDarkMode = mediaQueryList && mediaQueryList.matches;
+    console.log('Is it dark mode? ->',isDarkMode);
+    this.matchMedia.isDarkModeEnabled = isDarkMode;
+
+    //By cordova-plugin-theme-detection -> cordovaPlugin
+    cordova.plugins.ThemeDetection.isAvailable((res)=>{
+      this.cordovaPlugin.isAvailable = JSON.stringify(res);
+    },(err)=>{
+      this.cordovaPlugin.isAvailable = JSON.stringify(err);
+    });
+    cordova.plugins.ThemeDetection.isDarkModeEnabled((res)=>{
+      this.cordovaPlugin.isDarkModeEnabled = JSON.stringify(res);
+    },(err)=>{
+      this.cordovaPlugin.isDarkModeEnabled = JSON.stringify(err);
+    });
   }
 
 }
